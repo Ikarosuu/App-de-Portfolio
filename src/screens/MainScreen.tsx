eefe90d08f60eb93a39809ab1138370dddae4413
ikarosuu <icaro.code@proton.me>
2025-09-10 13:47:43 -0400

    first commit

---
+++ src/screens/MainScreen.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+
+export default function MainScreen() {
+    return(
+        <View>
+            <Text>Main Work!</Text>
+            <StatusBar style='auto'/>
+        </View>
+    )
+}
---
+++ src/screens/SkillScreen.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+
+export default function SkillScreen() {
+    return(
+        <View>
+            <Text>Skill Work!</Text>
+            <StatusBar style='auto'/>
+        </View>
+    )
+}
